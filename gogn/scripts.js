e7c3a4d927f7b3a65d73271c51563e5296b73e4c
jonnigs <jonnigs@gmail.com>
2017-11-04 16:36:30 +0000

    Allt komið
    Þarf að commita oftar

--- gogn/scripts.js
+++ gogn/scripts.js
@@ -6,30 +6,33 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 var text = (function() {
-  // skilgreina "private" breytur sem við notum í forriti
-  var input;    // input fyrir fyrirsögn
-  var textarea; // textarea fyrir texta
+  var form = document.querySelector('#form');
+  var input = form.querySelector('.heading input');    // input fyrir fyrirsögn
+  var textarea = form.querySelector('.textarea textarea'); // textarea fyrir texta
 
-  var textContainer;  // div sem inniheldur texta
-  var errorContainer; // div sem inniheldur villuskilaboð
+  var textContainer = document.querySelector('.texts');  // div sem inniheldur texta
+  var errorContainer = form.querySelector('.error'); // div sem inniheldur villuskilaboð
 
-  var invert;     // checkbox fyrir hvort eigi að vera inverted
-  var uppercase;  // checkbox fyrir hvort fyrirsögn eigi að vera í hástöfum
+  var invert = form.querySelector('.checkbox .invert');     // checkbox fyrir hvort eigi að vera inverted
+  var uppercase = form.querySelector('.checkbox .uppercase');  // checkbox fyrir hvort fyrirsögn eigi að vera í hástöfum
 
-  /**
-   * Fall sem sér um að athuga hvort form sé rétt útfyllt:
-   * - Athugar hvort fyrirsögn sé gild
-   * - Athugar hvort texti sé gildur
-   * - Ef villur eru til staðar, bætir ekki við texta en
-   *   + Setur villutexta m.v. hvað er vitlaust
-   *   + Setur "invalid" class á reit sem er vitlaust
-   * - Ef engar villur, bætir við texta og
-   *   + Fjarlægir villutexta
-   *   + Fjarlægir "invalid" class af öllu
-   *   + Hreinsar form
-   */
+
   function submit(e) {
-    e.preventDefault();
+    e.preventDefault(); // Stoppar síðuna í að reloada
+
+    var valid = validHeading(input.value.length); // Tékkar hvort fyrirsögnin sé eins og við viljum
+    if (valid == true){
+      valid = validText(textarea.value.length); // Tékkar hvort að textinn sé eins og við viljum
+      if (valid == true){
+        addText(input.value, textarea.value, invert.checked, uppercase.checked); // Bætir textanum við ef allt er í lagi
+        setError(""); // Hreinsar villuskilaboðin
+        clearForm(); // Hreinsa formið
+      } else {
+        setError("Texti má ekki vera tómur eða yfir 1.000 stafa langur."); // Setur villuskilaboð ef texti  er ekki réttur
+      }
+    } else {
+      setError("Fyrirsögn má ekki vera tóm eða yfir 100 stafi löng."); // Setur villuskilaboð ef fyrirsögn er ekki rétt
+    }
   }
 
   /**
@@ -37,18 +40,29 @@ var text = (function() {
    * Fjarlægir villutexta ef text er tómistrengur.
    */
   function setError(text) {
+    form.querySelector('.error').textContent = text;
   }
 
   /**
    * Staðfestir að fyrirsögn sé rétt. Skilar true ef lengd er > 0 og <= 100
    */
   function validHeading(value) {
+    if (value > 0 && value <= 100) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /**
    * Staðfestir að texti sé réttur. Skilar true ef lengd er > 0 og <= 1000
    */
   function validText(value) {
+    if (value > 0 && value <= 1000) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /**
@@ -58,13 +72,36 @@ var text = (function() {
    * Ef isUppercase er true er "uppercase" class bætt við h2
    */
   function addText(heading, text, isInverted, isUppercase) {
+    var div = document.createElement('div');
+    div.className = "text";
+
+    var fyrirsogn = document.createElement('h2');
+    fyrirsogn.textContent = heading;
+    if (isUppercase == true){
+      fyrirsogn.className = "uppercase";
+    }
+
+    var texti = document.createElement('p');
+    texti.textContent = text;
+    if (isInverted == true) {
+      div.className = "text inverted";
+    }
+
+    var textsdiv = document.querySelector('.texts');
+    div.appendChild(fyrirsogn);
+    div.appendChild(texti);
+    textsdiv.appendChild(div);
   }
 
   /**
-   * Sér um að hreins form, taka texta úr input og textarea, inverted og
+   * Sér um að hreinsa form, taka texta úr input og textarea, inverted og
    * uppercase sett sem ótjékkuð
    */
   function clearForm() {
+    form.querySelector('.heading input').value = "";
+    form.querySelector('.textarea textarea').value = "";
+    form.querySelector('.checkbox .invert').checked = false;
+    form.querySelector('.checkbox .uppercase').checked = false;
   }
 
   /**
@@ -73,8 +110,19 @@ var text = (function() {
    */
   function init(form, texts) {
     input = form.querySelector('.heading input');
+    textarea = form.querySelector('.textarea textarea');
 
-    console.log('input fannst sem', input);
+    textContainer = texts;
+    errorContainer = form.querySelector('.error');
+
+    invert = form.querySelector('.checkbox .invert');
+    uppercase = form.querySelector('.checkbox .uppercase');
+
+    var takki = form.querySelector('button');
+
+    takki.addEventListener('click',function(e){
+      submit(e);
+    });
   }
 
   return {
